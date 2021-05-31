433418f51d723ceada014dde5983c9d637a0340c
Tyler <tyler@tkhatch.com>
2021-05-31 11:13:46 -0500

    initial call to api

--- global.d.ts
+++ global.d.ts
@@ -2,6 +2,7 @@ namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test',
 
+    REACT_APP_API_URL: string,
     REACT_APP_AUTH0_CLIENTID: string,
     REACT_APP_AUTH0_DOMAIN: string,
   }
